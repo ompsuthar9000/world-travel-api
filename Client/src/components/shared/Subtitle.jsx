58e3f096ceb23a57e17a9dd2f2d030cd89673164
ompsuthar9000 <91866686+ompsuthar9000@users.noreply.github.com>
2024-01-15 20:51:16 +0530

    Delete Client directory

--- Client/src/components/shared/Subtitle.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Subtitle = ({ Subtitle }) => {
-  return <h3 className="section__subtitle">{Subtitle}</h3>;
-};
-
-export default Subtitle;
--- Client/src/components/utils/config.js
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = "https://server-tau-brown.vercel.app/api/v1";
